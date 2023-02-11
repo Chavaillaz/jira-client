3ba556ae22a6d23e642921d1f8f9bc1ea01d02df
Johan Chavaillaz <j.chavaillaz@gmail.com>
2023-02-11 19:55:05 +0100

    Make TestExecution implements list of TestStatus.

--- src/main/java/com/chavaillaz/jira/domain/TestExecution.java
+++ src/main/java/com/chavaillaz/jira/domain/TestExecution.java
@@ -1,28 +1,52 @@
 package com.chavaillaz.jira.domain;
 
+import static com.fasterxml.jackson.annotation.JsonFormat.Shape.OBJECT;
+
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonMerge;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.experimental.Delegate;
 
 /**
  * Part of the XRay plugin
  */
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+@JsonFormat(shape = OBJECT)
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TestExecution {
+public class TestExecution implements List<TestExecution.TestStatus> {
 
     private int count;
 
+    @Delegate
+    @JsonMerge
     private List<TestStatus> statuses;
 
+    /**
+     * Creates a new list of tests.
+     *
+     * @param testStatuses The tests to include in the list
+     * @return The corresponding list of tests
+     */
+    public static TestExecution from(TestStatus... testStatuses) {
+        return from(List.of(testStatuses));
+    }
+
+    /**
+     * Creates a new list of tests.
+     *
+     * @param testStatuses The tests to include in the list
+     * @return The corresponding list of tests
+     */
+    public static TestExecution from(List<TestStatus> testStatuses) {
+        TestExecution container = new TestExecution();
+        container.addAll(testStatuses);
+        return container;
+    }
+
     @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class TestStatus {
 
@@ -41,6 +65,20 @@ public class TestExecution {
         private int statusCount;
 
         private float statusPercent;
+
+        /**
+         * Creates a new test status.
+         *
+         * @param name  The name of the status
+         * @param count The number of elements corresponding to this status
+         * @return The corresponding test status
+         */
+        public static TestStatus from(String name, int count) {
+            TestStatus test = new TestStatus();
+            test.setName(name);
+            test.setStatusCount(count);
+            return test;
+        }
 
     }
 
